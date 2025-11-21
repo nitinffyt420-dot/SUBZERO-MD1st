42a67dde95c0adccfca12f2d8228a30a67c7564d
nitinffyt420-dot <nitinffyt420@gmail.com>
2025-11-21 23:20:24 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "Ice~OLJX7i", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "916001031685", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
